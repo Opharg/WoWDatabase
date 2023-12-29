5b3c2742ec32e76e6a243fd1b9a1b77ecb4c5273
Opharg <jonathan.stolte@web.de>
2023-12-29 02:46:33 +0100

    get community-listfile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from urllib.request import urlopen
 import os
 import shutil
 import sys
@@ -10,6 +11,26 @@ import git
 import dbdefs
 import parser_logger
 
+
+def update_listfile():
+    url = r'https://github.com/wowdev/wow-listfile/releases/latest/download/community-listfile-withcapitals.csv'
+    file_path = r'./community-listfile.csv'
+    try:
+        # Open the URL
+        with urlopen(url) as response, open(file_path, 'wb') as out_file:
+            # Read the content in chunks and save it to the file
+            chunk_size = 4096
+            while True:
+                chunk = response.read(chunk_size)
+                if not chunk:
+                    break
+                out_file.write(chunk)
+
+        print(f"File downloaded successfully and saved as {file_path}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # argparse
@@ -18,6 +39,7 @@ if __name__ == '__main__':
     debug.add_argument('--debug', help='set logging to debug', action='store_true')
     debug.add_argument('--cdebug', help='set logging to debug and show debug messages in console', action='store_true')
     parser.add_argument('-b', type=str, help='set build number', required=True)
+    parser.add_argument("--listfile", help="Update Listfile", action="store_true")
     args = parser.parse_args()
 
     # logging
@@ -47,6 +69,18 @@ if __name__ == '__main__':
         wowdbdefs = git.Git('./WoWDBDefs')
         print('imported WoWDBDefs. Rerun program')
         sys.exit(0)
+
+    if not os.path.isfile('./community-listfile.csv'):
+        print('Downloading community-listfile')
+        update_listfile()
+    else:
+        print('community-listfile found')
+    if args.listfile:
+        print('Updating community-listfile')
+        print('Renaming community-listfile.csv -> community-listfile.csv.old')
+        os.rename('./community-listfile.csv', './community-listfile.csv.old')
+        update_listfile()
+        pass
 
     definitions = dbdefs.read_definitions_folder('./WoWDBDefs/definitions')
     definitions_build = dbdefs.get_definitions_by_build('./WoWDBDefs/definitions', '10.2.0.51239')
